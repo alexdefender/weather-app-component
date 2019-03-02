4fb807dca0db3829ea53d32e2633489d66e0a4eb
alex <alex.iv1102@gmail.com>
2019-03-02 09:01:28 +0200

    changed css class names

--- src/index.js
+++ src/index.js
@@ -1,2 +1,12 @@
-import './scss/style.scss';
-import 'normalize.css';
+import "./scss/style.scss";
+import "normalize.css";
+
+const CITY = new URL('http://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=9e5f732b51c403196a6a5096551e6098');
+
+let response = fetch(CITY);
+// let data = response.json();
+
+console.log(response);
+
+
+console.log(response.PromiseValue);
