16f294f42a0d6c47f8451962e57a1cce872739eb
Alex <alexiv1102@gmail.com>
2019-04-09 16:19:38 +0300

    remove cities from history

--- src/js/Components/Header/SearchHistory/SearchHistory.js
+++ src/js/Components/Header/SearchHistory/SearchHistory.js
@@ -9,13 +9,18 @@ export default class SearchHistory extends SearchBar {
     }
 
     init() {
-        ['updateMySelf']
+        ['updateMySelf', 'removeCityByClickBtn']
             .forEach(methodName => this[methodName] = this[methodName].bind(this));
     }
 
     updateMySelf(state) {
         if (!this.favState.includes(state)) this.favState.push(state);
-        this.updateState(this.state);
+        this.updateState(this.favState);
+    }
+
+    removeCityByClickBtn(e) {
+        this.favState = this.favState.filter(city => city !== e.target.id);
+        this.updateState(this.favState);
     }
 
     render() {
@@ -27,6 +32,15 @@ export default class SearchHistory extends SearchBar {
                     children: [
                         {
                             tag: "button",
+                            eventHandlers: {
+                                click: this.removeCityByClickBtn,
+                            },
+                            attributes: [
+                                {
+                                    name: "id",
+                                    value: city
+                                },
+                            ],
                             classList: ["fa", "fa-times"]
                         }
                     ]
