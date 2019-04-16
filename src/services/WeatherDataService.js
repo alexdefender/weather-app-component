5a5b92c6f861a0061645c151314bddb07c6e76c6
Alex <alexiv1102@gmail.com>
2019-04-16 06:24:04 +0300

    replacement http to https

--- src/services/WeatherDataService.js
+++ src/services/WeatherDataService.js
@@ -1,8 +1,8 @@
 import AppState from "./AppState";
 
 const API_KEY = "&APPID=9e5f732b51c403196a6a5096551e6098";
-const API_DATA_WEATHER = "http://api.openweathermap.org/data/2.5/weather?q=";
-const API_DATA_FORECAST = "http://api.openweathermap.org/data/2.5/forecast?q=";
+const API_DATA_WEATHER = "https://api.openweathermap.org/data/2.5/weather?q=";
+const API_DATA_FORECAST = "https://api.openweathermap.org/data/2.5/forecast?q=";
 let API_TEMP_UNIT = "&units=metric";
 
 class WeatherDataService {
